f6d924a26a73e2caf30326e3cdb6711a626fa7e6
aaravcorporation <aaravcorporation24@gmail.com>
2026-08-13 19:17:00 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aarav-stock-manager-v9-product-edit-delete';;;;;
+const CACHE='aarav-stock-manager-v10-product-master-tab';;;;;;
 const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
